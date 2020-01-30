c835d2d579da64b20ceda80e719737cb9fb3b63f
Richard Menzies <rdmenzies2000@gmail.com>
2020-01-30 18:52:19 +0000

    Finished chapter 4

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def index(request):
     content_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=content_dict)
-    #return HttpResponse("Rango says hi, here is the link to the about page: <a href='/rango/about'>about</a>")
 
 def about(request):
-    return HttpResponse("This is the about page, here is the link to the index page: <a href='/rango/'>Index</a>")
+    content_dict = {'name': 'Richard Menzies'}
+    return render(request, 'rango/about.html', context = content_dict)
