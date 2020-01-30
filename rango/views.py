a762794b39c146106e875c315061e875abdd9c6a
Richard Menzies <rdmenzies2000@gmail.com>
2020-01-30 17:41:48 +0000

    Fixing a few things so test cases for chapter 3 pass

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,4 @@ def index(request):
     #return HttpResponse("Rango says hi, here is the link to the about page: <a href='/rango/about'>about</a>")
 
 def about(request):
-    return HttpResponse("This is the about page, here is the link to the index page: <a href='/rango/'>index</a>")
+    return HttpResponse("This is the about page, here is the link to the index page: <a href='/rango/'>Index</a>")
