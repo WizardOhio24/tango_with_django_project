b291d80f1bb110577a6137ca5868be831015f73b
WizardOhio24 <rdmenzies2000@gmail.com>
2020-02-11 10:00:47 +0000

    Finished task 6: implimented most viewed pages and most liked categories list on the index page

--- populate_rango.py
+++ populate_rango.py
@@ -10,27 +10,36 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url': 'http://docs.python.org/3/tutorial'},
+        'url': 'http://docs.python.org/3/tutorial',
+        'views': '50'},
         {'title':'How to Think like a Computer Scientist',
-        'url': 'http://www.greenteapress.com/thinkpython/'},
+        'url': 'http://www.greenteapress.com/thinkpython/',
+        'views': '100'},
         {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'
+        'url':'http://www.korokithakis.net/tutorials/python/',
+        'views': '100'
         }]
 
     django_pages = [
             {'title':'Official Django Tutorial',
-            'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+            'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views': '100'},
             {'title':'Django Rocks',
-            'url':'http://www.djangorocks.com/'},
+            'url':'http://www.djangorocks.com/',
+            'views': '100'},
             {'title':'How to Tango with Django',
-             'url':'http://www.tangowithdjango.com/'} 
+             'url':'http://www.tangowithdjango.com/',
+             'views': '100'} 
         ]
 
     other_pages = [
         {'title':'Bottle',
-        'url':'http://bottlepy.org/docs/dev/'},
+        'url':'http://bottlepy.org/docs/dev/',
+        'views': '100'
+        },
         {'title':'Flask',
-        'url':'http://flask.pocoo.org'} ]
+        'url':'http://flask.pocoo.org',
+        'views': '100'} ]
 
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
@@ -49,7 +58,7 @@ def populate():
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
-    p.views = views
+    p.views = views if views else p.views 
     p.save()
     return p
 
--- rango/admin.py
+++ rango/admin.py
@@ -5,7 +5,7 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 class CategoryAdmin(admin.ModelAdmin):
-    proproulates_fields = {'slug'; {'name',}}
+    proproulates_fields = {'slug': ('name',)}
 
 admin.site.register(Page, PageAdmin)
 admin.site.register(Category, CategoryAdmin)
