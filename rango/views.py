b285a84b7d65b702ab3d45302fb487189ba07b65
WizardOhio24 <rdmenzies2000@gmail.com>
2020-02-12 20:54:38 +0000

    Finished Chapter 8, passing all tests

--- rango/views.py
+++ rango/views.py
@@ -50,7 +50,7 @@ def add_category(request):
         if form.is_valid():
             print("Commiteted")
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
@@ -62,7 +62,7 @@ def add_page(request, category_name_slug):
     except Category.DoesNotExist:
         category = None
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
     form = PageForm()
     if request.method == 'POST':
         form = PageForm(request.POST)
