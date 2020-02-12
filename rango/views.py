f9191c92f27609cba4947cf6686ebdf842936260
WizardOhio24 <rdmenzies2000@gmail.com>
2020-02-12 23:06:41 +0000

    Added login functionality

--- rango/views.py
+++ rango/views.py
@@ -13,6 +13,13 @@ from rango.forms import PageForm
 
 from rango.forms import UserForm, UserProfileForm
 
+from django.contrib.auth import authenticate, login
+
+from django.contrib.auth.decorators import login_required
+
+
+
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -108,3 +115,29 @@ def register(request):
         profile_form = UserProfileForm()
 
     return render(request, 'rango/register.html', context = {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+       
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+return HttpResponse("Since you're logged in, you can see this text!")
+
